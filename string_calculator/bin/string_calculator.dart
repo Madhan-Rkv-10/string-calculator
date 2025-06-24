65bf7936a11414afba59fd19db26558f2bc8ca89
Madhan-Rkv-10 <madhanrkv10@gmail.com>
2025-06-24 08:16:47 +0530

    chore: add  exception handling for calculator

--- string_calculator/bin/string_calculator.dart
+++ string_calculator/bin/string_calculator.dart
@@ -8,7 +8,11 @@ void main(List<String> arguments) {
   print(calculator.add("1\n5\n7"));
   print(calculator.add("//;\n1;2;5;3"));
   print(calculator.add("//*\n1*25*3"));
-  print(calculator.add('1,-2,3'));
+  try {
+    print(calculator.add('1,-2,3'));
+  } catch (e) {
+    print(e);
+  }
   print(calculator.add("2,1001"));
   print(calculator.add("//[***]\n1***2***3"));
   print(calculator.add("//[*][%]\n1*4%3"));
